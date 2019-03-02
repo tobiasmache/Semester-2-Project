3357e686370c3656fa74df2865785ec77fbee0d9
Daw <daw2744@icloud.com>
2019-03-01 19:32:20 -0800

    'sWork: Auto stash before merge of "Dylan'sWork" and "origin/Dylan'Work"

--- Assets/PennyPixel/Scripts/PlayerPlatformerController.cs
+++ Assets/PennyPixel/Scripts/PlayerPlatformerController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+/// <summary>
+//Testing chagnes
+/// </summary>
 
 public class PlayerPlatformerController : PhysicsObject {
 
