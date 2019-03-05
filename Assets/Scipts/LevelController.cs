5de53999caaab8b22eecffebee375ff30b202cb8
Tobias Mache <tobyma23@gmail.com>
2019-03-05 11:10:36 -0800

    LightingMVP

--- Assets/Scipts/LevelController.cs
+++ Assets/Scipts/LevelController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
 
 public class LevelController : MonoBehaviour {
 
@@ -12,6 +13,29 @@ public class LevelController : MonoBehaviour {
         if (other.CompareTag("Player"))
         {
             SceneManager.LoadScene(newLevel);
+        }
+    }
+
+    public Tilemap DarkMap;
+    public Tilemap DimMap;
+    public Tilemap BackgroundMap;
+
+    public Tile DarkTile;
+    public Tile DimTile;
+
+    private void Start()
+    {
+        DarkMap.origin = DimMap.origin=BackgroundMap.origin;
+        DarkMap.size = DimMap.size=BackgroundMap.size;
+
+        foreach (Vector3Int p in DarkMap.cellBounds.allPositionsWithin)
+        {
+            DarkMap.SetTile(p, DarkTile);
+        }
+
+        foreach (Vector3Int p in DimMap.cellBounds.allPositionsWithin)
+        {
+            DimMap.SetTile(p, DimTile);
         }
     }
 }
